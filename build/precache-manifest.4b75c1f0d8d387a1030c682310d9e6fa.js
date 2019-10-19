4a43116c407bfd6a6558f6bf0ee84f051d728332
Hob Adams <hello@hobadams.com>
2019-10-19 10:12:54 +0100

    commit build direcotry

---
+++ build/precache-manifest.4b75c1f0d8d387a1030c682310d9e6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4221dfef12048b9a58dc79b63f5a9043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ac403e3ebc10150733",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bbc70f59f6c7a74d41a5",
+    "url": "/static/js/2.e2274ca2.chunk.js"
+  },
+  {
+    "revision": "a2ac403e3ebc10150733",
+    "url": "/static/js/main.a762b077.chunk.js"
+  },
+  {
+    "revision": "c5e649138e963642de4f",
+    "url": "/static/js/runtime-main.59f2fef9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
