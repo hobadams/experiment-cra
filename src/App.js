ff96eb59023afbe081c912b0cefce66a4a39465a
Hob Adams <hello@hobadams.com>
2019-10-19 11:04:34 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import About from './routes/About';
+import Home from './routes/Home';
+
+import Navigation from './components/Navigation';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const App = () => (
+  <div className="App">
+    <Router>
+      <header>
+        <div>
+          <Navigation />
+        </div>
       </header>
-    </div>
-  );
-}
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    NavLink
+} from "react-router-dom";
+
+export default function Navigation() {
+    return (
+        <ul>
+            <li>
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to="/about">About</NavLink>
+            </li>
+        </ul>
+
+    );
+}
